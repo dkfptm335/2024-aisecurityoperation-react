734363c1371d548d9218b35d5f270b1a72d85b7f
Gloomy <dkfptm335@naver.com>
2024-05-16 13:50:29 +0900

    Add custom arrows of gallery

--- src/pages/curriculum.tsx
+++ src/pages/curriculum.tsx
@@ -3,29 +3,29 @@ import {PageTemplate} from "./main";
 import {useState} from "react";
 
 export const Curriculum = () => {
-    const [className, setClassName] = useState(1);
+    const [curriName, setCurriName] = useState(1);
 
     return (
         <Wrap>
             <ClassWrap>
                 <button
-                    onClick={() => setClassName(1)}
-                    style={{backgroundColor: className === 1 ? 'black' : 'white', color: className === 1 ? 'white' : 'black'}}
+                    onClick={() => setCurriName(1)}
+                    style={{backgroundColor: curriName === 1 ? 'black' : 'white', color: curriName === 1 ? 'white' : 'black'}}
                 >1학년
                 </button>
                 <button
-                    onClick={() => setClassName(2)}
-                    style={{backgroundColor: className === 2 ? 'black' : 'white', color: className === 2 ? 'white' : 'black'}}
+                    onClick={() => setCurriName(2)}
+                    style={{backgroundColor: curriName === 2 ? 'black' : 'white', color: curriName === 2 ? 'white' : 'black'}}
                 >2학년</button>
                 <button
-                    onClick={() => setClassName(3)}
-                    style={{backgroundColor: className === 3 ? 'black' : 'white', color: className === 3 ? 'white' : 'black'}}
+                    onClick={() => setCurriName(3)}
+                    style={{backgroundColor: curriName === 3 ? 'black' : 'white', color: curriName === 3 ? 'white' : 'black'}}
                 >3학년</button>
             </ClassWrap>
             <ContentWrap>
-                { className === 1 ? <img src="assets/gallery/0.jpeg" alt="0"/> : null }
-                { className === 2 ? <img src="assets/gallery/1.jpg" alt="1"/> : null }
-                { className === 3 ? <img src="assets/gallery/2.jpeg" alt="2"/> : null }
+                { curriName === 1 ? <img src="assets/gallery/0.jpeg" alt="0"/> : null }
+                { curriName === 2 ? <img src="assets/gallery/1.jpg" alt="1"/> : null }
+                { curriName === 3 ? <img src="assets/gallery/2.jpeg" alt="2"/> : null }
             </ContentWrap>
         </Wrap>
     )
