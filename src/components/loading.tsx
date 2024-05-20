f5a7b4530c3003ff09d1bccb7ad6372fb03b712d
Gloomy <dkfptm335@naver.com>
2024-05-20 15:40:34 +0900

    feat: Loading 컴포넌트에 문구 추가

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import {GridLoader} from 'react-spinners';
+import './style.css'
 
 export const Loading = () => {
     return (
-        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
-            <GridLoader color={'#0165B0'} size={20} />
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            flexDirection: 'column',
+            fontFamily: 'Noto Sans KR',
+        }}>
+            <h1 style={{
+                color: '#0165B0',
+                fontSize: '2rem',
+                fontWeight: 'bold',
+            }}>페이지 불러오는 중...
+            </h1>
+            <GridLoader color={'#0165B0'} size={15} />
         </div>
     );
 }
